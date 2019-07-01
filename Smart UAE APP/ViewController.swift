518b592c6ab018270876cb54571a1bf23bb7f344
Nnamdi Ugwuoke <ugwuokebosco@gmail.com>
2019-07-01 14:27:00 +0400

    Initial Commit

---
+++ Smart UAE APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart UAE APP
+//
+//  Created by Nnamdi Ugwuoke on 7/1/19.
+//  Copyright © 2019 Smart UAE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
